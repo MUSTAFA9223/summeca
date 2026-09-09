c24999ca27b9e0fd04df75208c2e504e00a00ad9
MUSTAFA9223 <myjh2439@gmail.com>
2026-09-10 01:12:33 +0300

    Disable caching on the auth entry page

--- src/app/sign-up-login-screen/page.tsx
+++ src/app/sign-up-login-screen/page.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import AuthScreen from '@/app/sign-up-login-screen/components/AuthScreen';
 
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
 export default function SignUpLoginPage() {
   return <AuthScreen />;
 }
