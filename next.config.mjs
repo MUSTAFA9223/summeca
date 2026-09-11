228122f8c55e4c56eceebeec3512133df1768a95
MUSTAFA9223 <myjh2439@gmail.com>
2026-09-11 17:30:14 +0300

    Allow the official Spline viewer CDN through CSP

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const contentSecurityPolicy = [
   "object-src 'none'",
   "frame-ancestors 'none'",
   "form-action 'self'",
-  "script-src 'self' 'unsafe-inline' blob: https://www.googletagmanager.com https://www.google-analytics.com https://unpkg.com",
+  "script-src 'self' 'unsafe-inline' blob: https://www.googletagmanager.com https://www.google-analytics.com https://unpkg.com https://cdn.spline.design",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "font-src 'self' data: https://fonts.gstatic.com",
   "img-src 'self' data: blob: https:",
